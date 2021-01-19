1de77c11efc44c5b56656fe34dcae8560122cab8
kkaliszewicz <kamila.kaliszewicz@gmail.com>
2021-01-19 17:07:39 +0100

    Added easy hyperparameter tuning

--- train1.py
+++ train1.py
@@ -1,25 +1,26 @@
-import json
 import os
 import datetime
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorboard.plugins.hparams import api as hp
 
 
 dataset_dir_base = '../data/MergedDataset/'
 dataset_paths = [dataset_dir_base + split for split in ["train","val","test"]]
-model_checkpoint_filepath = "checkpoints.h5"
-model_history_filepath = "history.json.txt"
-log_dir_base = "logs/fit/"
+model_checkpoint_filepath = "Models/checkpoint_{}.h5"
+log_dir_base = "logs/hparam_tuning/"  # "logs/fit/"
 
 img_height = 512
 img_width = img_height//2
 batch_size = 40
 epochs = 16
-learn_rate = 0.001
+
+HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([16, 32]))
+HP_LEARNING_RATE = hp.HParam('lrate', hp.Discrete([1e-4, 1e-5]))
 
 
 def get_generators():
@@ -57,7 +58,7 @@ def get_num_samples():
     return [get_num_of_all_files(split) for split in dataset_paths]
 
 
-def build_model():
+def build_model(hparams):
     conv_base = tf.keras.applications.ResNet50V2(
         include_top=False,
         weights='imagenet',
@@ -70,42 +71,38 @@ def build_model():
     model = tf.keras.Sequential()
     model.add(conv_base)
     model.add(Dense(256, activation='relu'))
-    model.add(Dense(32, activation='relu'))
-    model.add(Dense(16, activation='relu'))
+	model.add(Dense(hparams[HP_NUM_UNITS], activation='relu'))
     model.add(Dense(6, activation='softmax'))
 
     model.compile(
         loss='categorical_crossentropy',
-        optimizer=Adam(learning_rate=learn_rate),
+        optimizer=Adam(learning_rate=hparams[HP_LEARNING_RATE]),
         metrics=['accuracy'])
     model.summary()
     return model
 
 
-def train_model(save_history=True):
-    model = build_model()
+def train_model(hparams, log_id):
+    model = build_model(hparams)
     generators = get_generators()
     num_samples = get_num_samples()
-    log_dir = log_dir_base + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
-	
+    log_dir = log_dir_base + log_id
+
+    TB = TensorBoard(log_dir=log_dir, histogram_freq=1)
+    HP = hp.KerasCallback(log_dir, hparams)
+    ES = EarlyStopping(monitor='val_loss', patience=2)
+    MC = ModelCheckpoint(filepath=model_checkpoint_filepath.format(log_id), verbose=1, save_best_only=True)
+
     history = model.fit_generator(
         generators[0],
         steps_per_epoch=np.ceil(num_samples[0] / batch_size),
         epochs=epochs,
         validation_data=generators[1],
         validation_steps=np.ceil(num_samples[1] / batch_size),
-        #verbose = 2
-        callbacks=[tensorboard_callback,
-                   ModelCheckpoint(filepath=model_checkpoint_filepath, verbose=1, save_best_only=True)]
+        callbacks=[TB, HP, MC]
     )
 
-    if save_history:
-        save_history_to_file(history)
-
-
-def save_history_to_file(history):
-    json.dump(history.history, open(model_history_filepath, 'w'))
+    return test_model(model)
 
 
 def test_saved_model():
@@ -118,19 +115,42 @@ def test_model(trained_model):
     test_generator = test_datagen.flow_from_directory(
         dataset_paths[2],
         target_size=(img_height, img_width),
+		color_mode='rgb',
         batch_size=batch_size,
         class_mode='categorical')
 
-    score = trained_model.evaluate_generator(
+    loss, score = trained_model.evaluate_generator(
         test_generator,
         steps=test_generator.n // batch_size + 1,
         verbose=2,
         callbacks=[])
 
-    print(score)
-    print(trained_model.metrics_names)
+	return score
+
+
+def run(run_name, hparams):
+    with tf.summary.create_file_writer(log_dir_base + run_name).as_default():
+        hp.hparams(hparams)
+        accuracy = train_model(hparams, run_name)
+        tf.summary.scalar('accuracy', accuracy, step=1)
+
+
+def main():
+    session_num = 0
+    for num_units in HP_NUM_UNITS.domain.values:
+        for lrate in HP_LEARNING_RATE.domain.values:
+                hparams = {
+                    HP_NUM_UNITS: num_units,
+                    HP_LEARNING_RATE: lrate,
+                }
+                log_id = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+                log_id = "run-{}-{}".format(log_id, session_num)
+                print('--- Starting trial: {}'.format(log_id ))
+                print({h.name: hparams[h] for h in hparams})
+                run(log_id, hparams)
+                session_num += 1
 
 
 if __name__ == '__main__':
-    train_model()
-    #test_saved_model()
+    main()
+	
